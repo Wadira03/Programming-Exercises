7fe9fd9ae4507e782288b37d911f330b3dd934a8
Wadira03 <162891025+Wadira03@users.noreply.github.com>
2024-03-10 16:21:59 +0300

    Update hello world.c

--- hello world.c
+++ hello world.c
@@ -3,9 +3,9 @@
 
 int main()
 {
-    printf("my name is brian\n");
+    printf("my name is chelsea\n");
     printf("my course is civil engineering\n");
-    printf("my favourite team is inter milan\n");
-    printf("my favourite meal is chapati and beans\n");
+    printf("my favourite team is PSG\n");
+    printf("my favourite meal is rice and beans\n");
     return 0;
 }
