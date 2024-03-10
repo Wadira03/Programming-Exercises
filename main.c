f279f36a8da111637e92ab11de48cb577b2325ee
Wadira03 <162891025+Wadira03@users.noreply.github.com>
2024-03-10 16:35:43 +0300

    scanf

--- main.c
+++ main.c
@@ -3,15 +3,13 @@
 
 int main()
 {
-    int a, b, c, d;
-    printf("Enter the first value :");
-    scanf("%d" , &a);
-    printf("Enter the second value :");
-    scanf("%d" , &b);
-    printf("Enter the third value :");
-    scanf("%d" , &c);
-    d = a + b + c;
-    printf("%d + %d + %d = %d\n" , a, b, c, d);
+    int a, b, c;
+        printf("Enter the first value :");
+        scanf("%d" , &a);
+        printf("Enter the second value :");
+        scanf("%d" , &b);
+        c = a + b;
+        printf("%d + %d = %d\n" , a, b, c);
     return 0;
 }
 
